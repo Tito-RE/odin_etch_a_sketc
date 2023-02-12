90df34f8ef82e8c60061842ed22c53d1e2669950
Tito Ramirez <75504060+Tito-RE@users.noreply.github.com>
2023-02-12 11:53:43 -0600

    Add delete grid function

--- js/main.js
+++ js/main.js
@@ -1,15 +1,29 @@
-function paint(e) {
+//Change the color of the div from the event
+function changeDivColor(e) {
   e.target.classList.add("marked");
 }
 
-const container = document.querySelector('.container');
-
-for (let i = 0; i < 16; i++) {
-  let div = document.createElement('div');
-  div.classList.add("item");
-  div.addEventListener('mouseover', (event) => {
-    paint(event);
-  });
-  container.appendChild(div);
+//Create a grid of div's given quantity per side 
+function createGrid(quantity) {
+  for (let i = 0; i < quantity*4; i++) {
+    let div = document.createElement('div');
+    div.classList.add("item");
+    div.addEventListener('mouseover', (event) => {
+      changeDivColor(event);
+    });
+    container.appendChild(div);
+  }
 }
 
+//Delete the elements of the container element (grid)
+function deleteGrid() {
+  while (container.lastElementChild) {
+    container.removeChild(container.lastElementChild);
+  }
+}
+
+const container = document.querySelector('.container');
+createGrid(4);
+const gridSize = prompt("What's quantity of squares per side you want");
+deleteGrid();
+createGrid(gridSize);
